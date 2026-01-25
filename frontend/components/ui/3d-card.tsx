384ca26a2545b591cd1dfd9dd97cadb6e768503c
Devargho <devarghochakraborty7@gmail.com>
2026-01-25 11:55:46 +0530

    Frontend Revamp done

--- frontend/components/ui/3d-card.tsx
+++ frontend/components/ui/3d-card.tsx
@@ -101,7 +101,7 @@ export const CardItem = ({
     rotateZ = 0,
     ...rest
 }: {
-    as?: React.ElementType;
+    as?: any;
     children: React.ReactNode;
     className?: string;
     translateX?: number | string;
